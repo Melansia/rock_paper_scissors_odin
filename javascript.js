324d57dcb05e7088b2f4469f6ac80357ddf979f8
Melansia <26363056+Melansia@users.noreply.github.com>
2024-12-30 16:52:35 +0200

    fuction to get human choice as well as get a random choice formcomputer

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 console.log("Hello World")
+
+function getComputerChoice(choice) {
+    return choice[Math.floor((Math.random() * choice.length))]
+}
+
+function getHumanChoice() {
+    let choice = prompt ("What's your play, 'rock', 'paper' or 'scissors' ?")
+    return choice.toLocaleLowerCase()
+}
+
+
+
+
+function main() {
+    const options = ["rock", "paper", "scissors"]
+
+    console.log(getComputerChoice(options))
+    console.log(getHumanChoice())  
+}
+
+main()
